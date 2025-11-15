ec4a2357edb4dbee214328d8522319e56a4e42dd
Clive Chuma <99633678+chuma1999@users.noreply.github.com>
2025-11-15 18:32:39 +0200

    Correct CORS middleware usage giving access to any ip address
    Fixed CORS middleware configuration.

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://mern-task-app-frontend-q1v0.onrender.com"]
-  })
+  cors(()
 );
 app.use("/api/tasks", taskRoutes);
 
